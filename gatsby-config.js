112f06c943e664617f1c81f82059e9fb4e1554ee
Sajjad Dashti Nezhad <saggad.dashti@gmail.com>
2025-02-27 18:12:06 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ReccoUI`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
